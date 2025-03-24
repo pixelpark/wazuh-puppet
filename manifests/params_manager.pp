d91829fc525fde268bcaa00b94af0da00067192d
Vasko Mihaylov <vaskosmihaylov@gmail.com>
2025-03-24 13:10:39 +0200

    Fix package versions to use 4.11

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -5,7 +5,7 @@ class wazuh::params_manager {
     'Linux': {
 
     # Installation
-      $server_package_version                          = '4.11.2'
+      $server_package_version                          = '4.11.1'
 
       $manage_firewall                                 = false
 
